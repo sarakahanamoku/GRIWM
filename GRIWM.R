a0d37df34be275fbb4659315933fcd46f05c9237
Victor Tsang <35205232+victorwctsang@users.noreply.github.com>
2022-10-27 12:53:52 +1100

    Fixed typo in vector indexing

--- GRIWM.R
+++ GRIWM.R
@@ -16,7 +16,7 @@ GRIWM <- function(df){
     ## resampling of the standard deviation of each date from a Gaussian distribution
     for (ii in 1:k) {
       date_samp[ii] <-
-        round(rnorm(1, mean = as.numeric(dates[ii, ]), sd = as.numeric(sd[ii, ])))
+        round(rnorm(1, mean = as.numeric(dates[ii]), sd = as.numeric(sd[ii])))
     }
     date_samp <- (sort(date_samp))
 
